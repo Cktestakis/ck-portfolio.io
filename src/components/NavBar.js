6a8b2abb38a5deb3b82310ff318a515cd9698752
Cktestakis <codyktestakis@gmail.com>
2024-05-04 10:29:25 -0400

    Logo updated to cody.svg

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import cody from "../assets/img/cody.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 // import navIcon2 from "../assets/img/nav-icon2.svg";
 // import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -34,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <img src={cody} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
